e7bfba1ffa972b99e20d08d54365d6f4d55802f3
Michele Palmieri <mik.palmieri@katamail.com>
2015-05-30 01:39:22 +0200

    Revert "VER001"
    This reverts commit 1ea1e79400c56a768a1bc9bbbd77e3e7ab07a790.

--- cookieEU/lang/lang.it-it.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $lang['plugin']['cookieEU']['msg'] = 'Questo sito usa i cookies per migliorare la vostra esperienza di navigazione e per fini statistici. Nessun dato viene raccolto senza il consenso dell&#39;utente. Per maggiori informazioni andare nostra ';
-    $lang['plugin']['cookieEU']['btnmsg'] = 'Accetto i Cookies da questo sito.';
-    $lang['plugin']['cookieEU']['linkmsg'] = 'pagina della privacy..';
-?>
